cb008fac899102649046cb17eaba600009d3303f
Silverfeelin <Silverfeelin@gmail.com>
2019-06-29 02:07:29 +0200

    Update templates.

--- src/app/layout/tab/tab-back/back-file-templates.ts
+++ src/app/layout/tab/tab-back/back-file-templates.ts
@@ -2,20 +2,20 @@ import { IFileTemplate } from '@root/layout/template/file-template-interface';
 
 export const backFileTemplates: Array<IFileTemplate> = [
   {
-    asset: 'assets/templates/pants/pantsm.png',
-    pdn: 'assets/templates/pants/pantsm.pdn',
-    title: 'Pants (male)',
-    description: 'Pants frames file with male body frames.'
+    asset: 'assets/templates/back/backm.png',
+    pdn: 'assets/templates/back/backm.pdn',
+    title: 'Back (male)',
+    description: 'Back frames file with male body frames.'
   },
   {
-    asset: 'assets/templates/pants/pantsf.png',
-    pdn: 'assets/templates/pants/pantsf.pdn',
-    title: 'Pants (female)',
-    description: 'Chest frames file with female body frames.'
+    asset: 'assets/templates/back/backf.png',
+    pdn: 'assets/templates/back/backf.pdn',
+    title: 'Back (female)',
+    description: 'Back frames file with female body frames.'
   },
   {
-    asset: 'assets/templates/pants/pants_blank.png',
-    title: 'Pants (empty)',
-    description: 'Empty pants frames file.'
+    asset: 'assets/templates/back/back_blank.png',
+    title: 'Back (empty)',
+    description: 'Empty back frames file.'
   }
 ];
